dbe55bcb5d1df85e444ebd523ff771e76eb1288b
Opi22 <khideromer20031610@gmail.com>
2023-11-30 07:31:20 +0200

    styles and functions added

---
+++ script.js
@@ -0,0 +1,7 @@
+let answer = document.getElementById("result");
+let  problem=document.getElementById("problem");
+let btn1 = document.getElementById("btn1");
+function button1(){
+    answer.textContent+="1";
+}
+btn1.addEventListener("click", button1);
